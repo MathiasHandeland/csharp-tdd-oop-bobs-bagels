8381527a59defbec931e407ee9acf6d4a1256bf8
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-12 11:30:06 +0200

    added test for printing receipts: checking that something was actually printed

--- exercise.tests/ReceiptExtentionTests.cs
+++ exercise.tests/ReceiptExtentionTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using exercise.main;
+using exercise.main.Products;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,27 @@ namespace exercise.tests
 {
     public class ReceiptExtentionTests
     {
-        // [Test] 
-        // check that something was printed
+        [Test] // user story 11, printing receipt works
+        public void PrintReceipt()
+        {
+            // arrange
+            Inventory inventory = new Inventory();
+            Basket basket = new Basket(inventory);
+            basket.AddProduct(new Bagel("BGLO"));
+            basket.AddProduct(new Bagel("BGLO"));
+            Receipt receipt = new Receipt(basket);
+
+            // redirect console output
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            // act
+            receipt.Print();
+
+            // assert
+            string printed = output.ToString();
+            Assert.IsNotEmpty(printed); // Check that something actualy was printed
+            Assert.That(printed, Does.Contain("Bob's Bagels")); 
+        }
     }
 }
